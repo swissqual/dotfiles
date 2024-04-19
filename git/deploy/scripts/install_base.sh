b7e4d0f1cbe22b940c1d1d3205df8120fc71f29a
sqcloud <119507415+sqcloud@users.noreply.github.com>
2024-04-19 10:18:26 +0200

    Update install_base.sh
    Removed: chezmoi apply, just show versions

--- git/deploy/scripts/install_base.sh
+++ git/deploy/scripts/install_base.sh
@@ -7,13 +7,14 @@ sudo apt install python3 python3-pip git pipenv sshpass curl jq unzip -y
 # Install Bitwarden:
 curl -L -o /tmp/bw.zip https://github.com/bitwarden/cli/releases/download/v1.22.1/bw-linux-1.22.1.zip
 unzip /tmp/bw.zip -d /tmp/bw
-sudo mv /tmp/bw/bw /usr/local/bin
+sudo mv /tmp/bw/bw /usr/local/bin -f
 sudo chmod u+x /usr/local/bin/bw
 rm /tmp/bw.zip
+rm /tmp/bw
 
 # Install chezmoi
 sudo sh -c "$(curl -fsLS get.chezmoi.io)" -- -b /usr/local/bin
 
-# Init / apply chezmoi
-chezmoi init https://github.com/swissqual/dotfiles.git --branch sqml01-hetzner
-chezmoi apply
+# Show versions
+bw --version
+chezmoi --version
