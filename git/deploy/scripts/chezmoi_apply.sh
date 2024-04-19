d4b8ccb44cf6306f62299137c666c1f1eca5cec7
sqcloud <119507415+sqcloud@users.noreply.github.com>
2024-04-19 11:18:56 +0200

    Create chezmoi_apply.sh

---
+++ git/deploy/scripts/chezmoi_apply.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Login Bitwarden (interactive)
+bw config server https://f60bsjkzjhjy0dvj.myfritz.net
+export BW_SESSION=$(bw login norabuild@work --raw)
+
+# Init / apply chezmoi
+chezmoi init https://github.com/swissqual/dotfiles.git --branch sqml01-hetzner
+chezmoi apply
+
+# Bitwarden logout
+bw logout
+unset BW_SESSION
