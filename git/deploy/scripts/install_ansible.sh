8b0fbc5ff4067c227407937d01105b12ba62fd83
sqcloud <119507415+sqcloud@users.noreply.github.com>
2024-04-19 16:13:26 +0200

    Update install_ansible.sh
    Fixed alias

--- git/deploy/scripts/install_ansible.sh
+++ git/deploy/scripts/install_ansible.sh
@@ -3,7 +3,8 @@
 # Create Python deploy venv
 mkdir ~/.venvs
 export venvs=~/.venvs
-python3 -m venv $venvs/deploy
+alias python=python3
+python -m venv $venvs/deploy
 source $venvs/deploy/bin/activate
 
 # Install ansible
@@ -11,5 +12,5 @@ pip install ansible
 ansible-galaxy role install geerlingguy.docker
 
 # Show versions
-python3 --version
+python --version
 ansible --version
