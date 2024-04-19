71fb9b7b079903062341adc7be36fff3a6be25d3
sqcloud <119507415+sqcloud@users.noreply.github.com>
2024-04-19 16:02:37 +0200

    Update install_base.sh
    Removed Ansible

--- git/deploy/scripts/install_base.sh
+++ git/deploy/scripts/install_base.sh
@@ -14,18 +14,6 @@ rm /tmp/bw.zip
 # Install chezmoi
 sudo sh -c "$(curl -fsLS get.chezmoi.io)" -- -b /usr/local/bin
 
-# Install Python venvs
-mkdir ~/.venvs
-export venvs=~/.venvs
-python3 -m venv $venvs/deploy
-source $venvs/deploy/bin/activate
-
-# Install ansible
-pip install ansible
-ansible-galaxy role install geerlingguy.docker
-
 # Show versions
-python3 --version
-ansible --version
 bw --version
 chezmoi --version
