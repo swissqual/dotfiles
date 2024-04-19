94a61b3ff3c48d415402e0186affbed5804b5637
sqcloud <119507415+sqcloud@users.noreply.github.com>
2024-04-19 15:45:10 +0200

    Update install_base.sh
    Added Python venvs

--- git/deploy/scripts/install_base.sh
+++ git/deploy/scripts/install_base.sh
@@ -2,7 +2,7 @@
 
 # Install dependencies:
 sudo apt-get update
-sudo apt install python3 python3-pip git pipenv sshpass curl jq unzip -y
+sudo apt install python3 python3-pip git python3.11-venv sshpass curl jq unzip -y
 
 # Install Bitwarden:
 curl -L -o /tmp/bw.zip https://github.com/bitwarden/cli/releases/download/v1.22.1/bw-linux-1.22.1.zip
@@ -14,6 +14,13 @@ rm /tmp/bw.zip
 # Install chezmoi
 sudo sh -c "$(curl -fsLS get.chezmoi.io)" -- -b /usr/local/bin
 
+# Install Python venvs
+mkdir ~/.venvs
+export venvs=~/.venvs
+python3 -m venv $venvs/deploy
+source $venvs/deploy/bin/activate
+
 # Show versions
+python3 --version
 bw --version
 chezmoi --version
