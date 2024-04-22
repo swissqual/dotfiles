25e146b4e3546f9c2746d1f53b38e718d4984983
sqcloud <119507415+sqcloud@users.noreply.github.com>
2024-04-22 14:29:25 +0200

    Create install_tailscale.sh

---
+++ git/deploy/scripts/install_tailscale.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Add Tailscale’s package signing key and repository:
+curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/debian/bookworm.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
+
+#Install Tailscale:
+sudo apt-get update
+sudo apt-get install tailscale
