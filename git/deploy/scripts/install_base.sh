9219ba58c2d783afe9a923cd662bd9657615cfe6
sqcloud <119507415+sqcloud@users.noreply.github.com>
2024-05-30 10:31:06 +0200

    Update install_base.sh
    fixed bw permissions

--- git/deploy/scripts/install_base.sh
+++ git/deploy/scripts/install_base.sh
@@ -8,7 +8,7 @@ sudo apt install python3 python3-pip git python3.11-venv sshpass curl wget jq un
 curl -L -o /tmp/bw.zip https://github.com/bitwarden/cli/releases/download/v1.22.1/bw-linux-1.22.1.zip
 unzip /tmp/bw.zip -d /tmp/bw
 sudo mv /tmp/bw/bw /usr/local/bin -f
-sudo chmod u+x /usr/local/bin/bw
+sudo chmod 755 /usr/local/bin/bw
 rm /tmp/bw.zip
 
 # Install chezmoi
