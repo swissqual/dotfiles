97c8842a299ec8d4c876157194ee58af1e0212ce
sqcloud <119507415+sqcloud@users.noreply.github.com>
2024-07-28 14:55:12 +0200

    Update chezmoi_apply.sh
    zootac

--- git/deploy/scripts/chezmoi_apply.sh
+++ git/deploy/scripts/chezmoi_apply.sh
@@ -5,7 +5,7 @@ bw config server https://f60bsjkzjhjy0dvj.myfritz.net
 export BW_SESSION=$(bw login dvdex@solnet.ch --raw)
 
 # Init / apply chezmoi
-chezmoi init https://github.com/swissqual/dotfiles.git --branch zoorola
+chezmoi init https://github.com/swissqual/dotfiles.git --branch zootac
 chezmoi update
 
 # Bitwarden logout
