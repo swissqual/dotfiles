b38dbb2613a12bfab2e2e6e21ac7653f71d591eb
sqcloud <119507415+sqcloud@users.noreply.github.com>
2024-07-28 14:53:21 +0200

    Update chezmoi_apply.sh
    origin zoorola

--- git/deploy/scripts/chezmoi_apply.sh
+++ git/deploy/scripts/chezmoi_apply.sh
@@ -2,10 +2,10 @@
 
 # Login Bitwarden (interactive)
 bw config server https://f60bsjkzjhjy0dvj.myfritz.net
-export BW_SESSION=$(bw login norabuild@work --raw)
+export BW_SESSION=$(bw login dvdex@solnet.ch --raw)
 
 # Init / apply chezmoi
-chezmoi init https://github.com/swissqual/dotfiles.git --branch sqml01-hetzner
+chezmoi init https://github.com/swissqual/dotfiles.git --branch zoorola
 chezmoi update
 
 # Bitwarden logout
