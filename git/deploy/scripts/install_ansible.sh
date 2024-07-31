6e52a38a0551ceccd7528af40d47de4dfc111b05
sqcloud <119507415+sqcloud@users.noreply.github.com>
2024-07-31 07:16:41 +0200

    Update install_ansible.sh
    Removed deploy venv part

--- git/deploy/scripts/install_ansible.sh
+++ git/deploy/scripts/install_ansible.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# Create Python deploy venv
-mkdir ~/.venvs
-python3 -m venv ~/.venvs/deploy
-source ~/.venvs/deploy/bin/activate
-
 # Install ansible
 pip install ansible==9.8.0
 ansible-galaxy role install geerlingguy.docker
