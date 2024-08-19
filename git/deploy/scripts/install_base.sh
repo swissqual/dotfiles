ece8e1d2cb4dfdbf8536871c79ed1ee7958dd2a7
sqcloud <119507415+sqcloud@users.noreply.github.com>
2024-08-19 13:36:28 +0200

    Update install_base.sh
    added restic for backup

--- git/deploy/scripts/install_base.sh
+++ git/deploy/scripts/install_base.sh
@@ -2,7 +2,7 @@
 
 # Install dependencies:
 sudo apt-get update
-sudo apt install python3 python3-pip git python3.11-venv sshpass curl wget jq unzip -y
+sudo apt install python3 python3-pip git python3.11-venv sshpass curl wget jq unzip restic -y
 
 # Install Bitwarden:
 curl -L -o /tmp/bw.zip https://github.com/bitwarden/cli/releases/download/v1.22.1/bw-linux-1.22.1.zip
